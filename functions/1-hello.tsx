e35d57f7f728d1d9e68c7cf61d3f82a23b884a5e
Sean <sean@vestaridefair.com>
2021-03-27 13:58:17 -0700

    tsx

--- functions/1-hello.tsx
+++ functions/1-hello.tsx
@@ -1,8 +1,8 @@
 // domain/.netlify/functions/1-hello
-// exports
+// exportsddd
 // const person = { name: 'john' }
 
 exports.handler = async (event, context, cb) => ({
   statusCode: 200,
-  body: 'Our First Netlify Function Example',
+  body: 'Our First Netlify Function Example!!!!!!!!',
 });
